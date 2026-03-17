d294b92232165956eac304d6b12dd5eb88fc6208
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-03-17 17:11:38 +0200

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const formatToSum = (node) => {
+  if (typeof node !== 'string') {
+    return NaN;
+  }
+
+  return node.replace(/[^\d.]/g, '');
+};
+
+const sortList = (ul) => {
+  const listItems = [...ul.children];
+
+  listItems.sort((person1, person2) => {
+    const salary1 = person1.dataset.salary;
+    const salary2 = person2.dataset.salary;
+
+    return formatToSum(salary2) - formatToSum(salary1);
+  });
+
+  listItems.forEach((item) => list.append(item));
+};
+
+const getEmployees = (person) => {
+  return {
+    name: person.textContent,
+    position: person.dataset.position,
+    salary: person.dataset.salary,
+    age: person.dataset.age,
+  };
+};
+
+sortList(list);
+getEmployees(list);
